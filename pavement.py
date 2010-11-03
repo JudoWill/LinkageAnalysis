b290ae657fef53ec2e42d17c2f9da80a0ab81520
Will Dampier <judowill@gmail.com>
2010-11-03 15:38:20 -0400

    refactored the utility functions

--- pavement.py
+++ pavement.py
@@ -32,36 +32,11 @@ def new_run():
 def top_function():
     pass
 
-@ruffus.posttask(ruffus.touch_file(os.path.join(options.DATA_DIR, 'ListFiles', 'search_sentinal')))
 @ruffus.files(os.path.join(options.DATA_DIR, 'ListFiles', 'search_sentinal'),
               os.path.join(options.DATA_DIR, 'ListFiles', 'sequences.list'))
 def get_sequence_ids(in_file, out_file):
 
-    def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, db = 'nucleotide'):
 
-        POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=%s&'  % db
-        POST_URL += 'retmax=%i&' % BLOCK_SIZE
-        if START > 0:
-            POST_URL += 'retstart=%i&' % START
-
-        search_term = search_sent.replace(' ', '%20')
-        search_term = search_term.replace('-', '%20')
-        search_term = search_term.replace('+', '%20')
-        search_url = POST_URL + '&term=' + search_term
-
-        if recent_date:
-            time_delta = datetime.today()-recent_date
-            search_url += '&reldate=' + str(time_delta.days)
-
-        xml_data = urllib2.urlopen(search_url).read()
-
-        id_list = re.findall('<Id>(\d*)</Id>', xml_data)
-
-        if len(id_list) == BLOCK_SIZE:
-            return id_list + SearchNCBI(search_sent, recent_date = recent_date,
-                                          BLOCK_SIZE = BLOCK_SIZE, START = START+BLOCK_SIZE-1)
-        else:
-            return id_list
 
     search_query = '"Human immunodeficiency virus 1"[porgn] AND 100: 15000[SLEN]'
 
@@ -71,3 +46,59 @@ def get_sequence_ids(in_file, out_file):
         handle.write('\n'.join(id_list))
 
 
+@ruffus.files(os.path.join(options.DATA_DIR, 'ListFiles', 'sequences.list'),
+              os.path.join(options.DATA_DIR, 'RawSequences', 'download_sentinal'))
+def get_sequences(in_file, out_file):
+    pass
+
+
+
+
+
+
+
+######Utility Functions!
+
+def GetXML(ID_LIST, db = 'nucleotide'):
+
+    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi?db=%s' % db
+    RET_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=%s&query_key=1&mode=xml&rettype=full' % db
+
+    pmid_list = ','.join(map(lambda x: str(x), ID_LIST))
+    post_req_url = POST_URL + '&id=' + pmid_list
+
+    post_res = urllib2.urlopen(post_req_url).read()
+
+    web_env = re.findall('<WebEnv>(.*?)</WebEnv>', post_res)[0]
+
+    req_url = RET_URL + '&WebENV=' + web_env
+
+    xml_data = urllib2.urlopen(req_url).read()
+    return xml_data.decode('ascii', 'ignore')
+
+
+def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, db = 'nucleotide'):
+
+    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=%s&'  % db
+    POST_URL += 'retmax=%i&' % BLOCK_SIZE
+    if START > 0:
+        POST_URL += 'retstart=%i&' % START
+
+    search_term = search_sent.replace(' ', '%20')
+    search_term = search_term.replace('-', '%20')
+    search_term = search_term.replace('+', '%20')
+    search_url = POST_URL + '&term=' + search_term
+
+    if recent_date:
+        time_delta = datetime.today()-recent_date
+        search_url += '&reldate=' + str(time_delta.days)
+
+    xml_data = urllib2.urlopen(search_url).read()
+
+    id_list = re.findall('<Id>(\d*)</Id>', xml_data)
+
+    if len(id_list) == BLOCK_SIZE:
+        return id_list + SearchNCBI(search_sent, recent_date = recent_date,
+                                      BLOCK_SIZE = BLOCK_SIZE, START = START+BLOCK_SIZE-1)
+    else:
+        return id_list
