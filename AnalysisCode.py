29d716680d891ab60ae137487f96ff76e21f09f6
Will Dampier <judowill@gmail.com>
2010-11-05 15:25:46 -0400

    moved the "workhorse" functions outside of the pipline function

--- AnalysisCode.py
+++ AnalysisCode.py
@@ -7,6 +7,7 @@ from itertools import imap, islice
 from suds.client import Client
 from BeautifulSoup import BeautifulStoneSoup
 import argparse
+from Code.NCBIUtils import *
 
 DATA_DIR = 'Data'
 OUT_DIR = 'Results'
@@ -68,49 +69,11 @@ def get_sequences(in_file, out_file):
 
 ######Utility Functions!
 
-def GetSeqs(client, ID_LIST):
-
-    xml = client.service.run_eFetch(db = 'nucleotide', id = ','.join(ID_LIST))
-    soup = BeautifulStoneSoup(xml)
-    for seq in soup.findAll('gbseq'):
-        gi = None
-        for id in seq.findAll('gbseqid'):
-            if id.contents[0].startswith('gi'):
-                gi = id.contents[0].split('|')[1]
-        if gi is None:
-            continue
-
-        nt_seq = seq.find('gbseq_sequence').contents[0]
-        yield nt_seq, gi
 
 
 
 
-def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, db = 'nucleotide'):
 
-    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=%s&'  % db
-    POST_URL += 'retmax=%i&' % BLOCK_SIZE
-    if START > 0:
-        POST_URL += 'retstart=%i&' % START
-
-    search_term = search_sent.replace(' ', '%20')
-    search_term = search_term.replace('-', '%20')
-    search_term = search_term.replace('+', '%20')
-    search_url = POST_URL + '&term=' + search_term
-
-    if recent_date:
-        time_delta = datetime.today()-recent_date
-        search_url += '&reldate=' + str(time_delta.days)
-
-    xml_data = urllib2.urlopen(search_url).read()
-
-    id_list = re.findall('<Id>(\d*)</Id>', xml_data)
-    print len(id_list)
-    if len(id_list) >= BLOCK_SIZE-1:
-        return id_list + SearchNCBI(search_sent, recent_date = recent_date,
-                                      BLOCK_SIZE = BLOCK_SIZE, START = START+BLOCK_SIZE-1)
-    else:
-        return id_list
 
 def take(N, iterable):
     return list(islice(iterable, N))
--- Code/NCBIUtils.py
+++ Code/NCBIUtils.py
@@ -1,6 +1,7 @@
 import re, urllib2
 from datetime import datetime
 from itertools import islice
+from BeautifulSoup import BeautifulStoneSoup
 
 
 def take(N, iterable):
@@ -9,7 +10,7 @@ def take(N, iterable):
 
 def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, db = 'nucleotide'):
 
-    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=%s&'
+    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=%s&'  % db
     POST_URL += 'retmax=%i&' % BLOCK_SIZE
     if START > 0:
         POST_URL += 'retstart=%i&' % START
@@ -18,6 +19,7 @@ def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, 
     search_term = search_term.replace('-', '%20')
     search_term = search_term.replace('+', '%20')
     search_url = POST_URL + '&term=' + search_term
+
     if recent_date:
         time_delta = datetime.today()-recent_date
         search_url += '&reldate=' + str(time_delta.days)
@@ -25,9 +27,25 @@ def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, 
     xml_data = urllib2.urlopen(search_url).read()
 
     id_list = re.findall('<Id>(\d*)</Id>', xml_data)
-
-    if len(id_nums) == BLOCK_SIZE:
+    print len(id_list)
+    if len(id_list) >= BLOCK_SIZE-1:
         return id_list + SearchNCBI(search_sent, recent_date = recent_date,
                                       BLOCK_SIZE = BLOCK_SIZE, START = START+BLOCK_SIZE-1)
     else:
         return id_list
+
+
+def GetSeqs(client, ID_LIST):
+
+    xml = client.service.run_eFetch(db = 'nucleotide', id = ','.join(ID_LIST))
+    soup = BeautifulStoneSoup(xml)
+    for seq in soup.findAll('gbseq'):
+        gi = None
+        for id in seq.findAll('gbseqid'):
+            if id.contents[0].startswith('gi'):
+                gi = id.contents[0].split('|')[1]
+        if gi is None:
+            continue
+
+        nt_seq = seq.find('gbseq_sequence').contents[0]
+        yield nt_seq, gi
