f8103f72a4601c975e64d30cb1ffb0dd3db6f897
Will Dampier <judowill@gmail.com>
2010-11-05 16:00:18 -0400

    re-worked the implementation of the GetSeqs function

--- Code/NCBIUtils.py
+++ Code/NCBIUtils.py
@@ -2,13 +2,14 @@ import re, urllib2
 from datetime import datetime
 from itertools import islice
 from BeautifulSoup import BeautifulStoneSoup
+from suds.client import Client
 
 
 def take(N, iterable):
     return list(islice(iterable, N))
 
 
-def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, db = 'nucleotide'):
+def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 1000000, START = 0, db = 'nucleotide'):
 
     POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=%s&'  % db
     POST_URL += 'retmax=%i&' % BLOCK_SIZE
@@ -35,17 +36,34 @@ def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, 
         return id_list
 
 
-def GetSeqs(client, ID_LIST):
+def GetSeqs(ID_LIST, BLOCK_SIZE = 100):
 
-    xml = client.service.run_eFetch(db = 'nucleotide', id = ','.join(ID_LIST))
-    soup = BeautifulStoneSoup(xml)
-    for seq in soup.findAll('gbseq'):
-        gi = None
-        for id in seq.findAll('gbseqid'):
-            if id.contents[0].startswith('gi'):
-                gi = id.contents[0].split('|')[1]
-        if gi is None:
-            continue
+    def extract_sequences(xml):
+        soup = BeautifulStoneSoup(xml)
+        for seq in soup.findAll('gbseq'):
+            gi = None
+            for id in seq.findAll('gbseqid'):
+                if id.contents[0].startswith('gi'):
+                    gi = id.contents[0].split('|')[1]
+            if gi is None:
+                continue
 
-        nt_seq = seq.find('gbseq_sequence').contents[0]
-        yield nt_seq, gi
+            nt_seq = seq.find('gbseq_sequence').contents[0]
+            yield nt_seq, gi
+
+    wsdl_url = 'http://www.ncbi.nlm.nih.gov/entrez/eutils/soap/v2.0/efetch_seq.wsdl'
+    client = Client('http://www.ncbi.nlm.nih.gov/entrez/eutils/soap/v2.0/efetch_seq.wsdl',
+                    retxml = True)
+
+    items_left = len(ID_LIST)
+    iter_list = iter(ID_LIST)
+    block = take(BLOCK_SIZE, iter_list)
+    while block:
+        xml = client.service.run_eFetch(db = 'nucleotide', id = ','.join(block))
+        for seq, gi in extract_sequences(xml):
+            yield seq, gi
+        items_left -= len(block)
+        print 'Items left: %i' % items_left
+        block = take(BLOCK_SIZE, iter_list)
+
+
