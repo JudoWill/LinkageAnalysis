54c11d25f7eb4ec1305a331fd027653871c01d22
Will Dampier <judowill@gmail.com>
2010-11-05 14:49:34 -0400

    added a function for getting sequences

--- pavement.py
+++ pavement.py
@@ -3,6 +3,9 @@ import csv, os, os.path
 import ruffus
 import urllib2, re
 from datetime import datetime
+from itertools import imap, islice
+from suds.client import Client
+from BeautifulSoup import BeautifulStoneSoup
 
 options(
     DATA_DIR = 'Data',
@@ -28,7 +31,7 @@ def new_run():
     pass
 
 
-@ruffus.follows('get_sequence_ids')
+@ruffus.follows('get_sequences')
 def top_function():
     pass
 
@@ -41,15 +44,46 @@ def get_sequence_ids(in_file, out_file):
     search_query = '"Human immunodeficiency virus 1"[porgn] AND 100: 15000[SLEN]'
 
     id_list = SearchNCBI(search_query)
-
+    print len(id_list)
     with open(out_file, 'w') as handle:
         handle.write('\n'.join(id_list))
 
 
 @ruffus.files(os.path.join(options.DATA_DIR, 'ListFiles', 'sequences.list'),
               os.path.join(options.DATA_DIR, 'RawSequences', 'download_sentinal'))
+@ruffus.follows(get_sequence_ids)
 def get_sequences(in_file, out_file):
-    pass
+
+    dump_dir = os.path.join(options.DATA_DIR, 'RawSequences')
+    with open(in_file) as handle:
+        seq_ids = set([x.strip() for x in handle])
+
+    present = set([x.split('.')[0] for x in os.listdir(dump_dir)])
+    dl_seqs = seq_ids - present
+    seq_iter = iter(dl_seqs)
+
+    client = Client('http://www.ncbi.nlm.nih.gov/entrez/eutils/soap/v2.0/efetch_seq.wsdl',
+                    retxml = True)
+    block_size = 100
+    block = take(block_size, seq_iter)
+    count = 0
+    while block:
+        for seq, gi in GetSeqs(client, block):
+            with open(os.path.join(dump_dir, gi + '.gi'), 'w') as handle:
+                handle.write('>%s\n%s\n' % (gi, seq))
+        count += len(block)
+        print count, len(dl_seqs)
+        block = take(block_size, seq_iter)
+
+
+
+
+
+
+
+
+
+    sh('touch %s' % out_file)
 
 
 
@@ -59,22 +93,22 @@ def get_sequences(in_file, out_file):
 
 ######Utility Functions!
 
-def GetXML(ID_LIST, db = 'nucleotide'):
+def GetSeqs(client, ID_LIST):
 
-    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi?db=%s' % db
-    RET_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=%s&query_key=1&mode=xml&rettype=full' % db
+    xml = client.service.run_eFetch(db = 'nucleotide', id = ','.join(ID_LIST))
+    soup = BeautifulStoneSoup(xml)
+    for seq in soup.findAll('gbseq'):
+        gi = None
+        for id in seq.findAll('gbseqid'):
+            if id.contents[0].startswith('gi'):
+                gi = id.contents[0].split('|')[1]
+        if gi is None:
+            continue
 
-    pmid_list = ','.join(map(lambda x: str(x), ID_LIST))
-    post_req_url = POST_URL + '&id=' + pmid_list
+        nt_seq = seq.find('gbseq_sequence').contents[0]
+        yield nt_seq, gi
 
-    post_res = urllib2.urlopen(post_req_url).read()
 
-    web_env = re.findall('<WebEnv>(.*?)</WebEnv>', post_res)[0]
-
-    req_url = RET_URL + '&WebENV=' + web_env
-
-    xml_data = urllib2.urlopen(req_url).read()
-    return xml_data.decode('ascii', 'ignore')
 
 
 def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, db = 'nucleotide'):
@@ -96,9 +130,12 @@ def SearchNCBI(search_sent, recent_date = None, BLOCK_SIZE = 100000, START = 0, 
     xml_data = urllib2.urlopen(search_url).read()
 
     id_list = re.findall('<Id>(\d*)</Id>', xml_data)
-
-    if len(id_list) == BLOCK_SIZE:
+    print len(id_list)
+    if len(id_list) >= BLOCK_SIZE-1:
         return id_list + SearchNCBI(search_sent, recent_date = recent_date,
                                       BLOCK_SIZE = BLOCK_SIZE, START = START+BLOCK_SIZE-1)
     else:
         return id_list
+
+def take(N, iterable):
+    return list(islice(iterable, N))
