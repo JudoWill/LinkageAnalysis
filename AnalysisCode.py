36bbc11c407a0bf6a089e86c8e1628b2ce5134ad
Will Dampier <judowill@gmail.com>
2010-11-05 16:00:43 -0400

    added a "touch" function

--- AnalysisCode.py
+++ AnalysisCode.py
@@ -1,10 +1,8 @@
-from paver.easy import sh
 import csv, os, os.path
 import ruffus
 import urllib2, re
 from datetime import datetime
 from itertools import imap, islice
-from suds.client import Client
 from BeautifulSoup import BeautifulStoneSoup
 import argparse
 from Code.NCBIUtils import *
@@ -12,12 +10,15 @@ from Code.NCBIUtils import *
 DATA_DIR = 'Data'
 OUT_DIR = 'Results'
 
+def touch(fname, times = None):
+    with file(fname, 'a'):
+        os.utime(fname, times)
 
 def touch_data():
     for path, _, files in os.walk(DATA_DIR):
         for f in files:
             f = f.replace(' ', '\ ')
-            sh('touch %s' % os.path.join(path, f))
+            touch(os.path.join(path, f))
 
 
 
@@ -50,33 +51,13 @@ def get_sequences(in_file, out_file):
 
     present = set([x.split('.')[0] for x in os.listdir(dump_dir)])
     dl_seqs = seq_ids - present
-    seq_iter = iter(dl_seqs)
-
-    client = Client('http://www.ncbi.nlm.nih.gov/entrez/eutils/soap/v2.0/efetch_seq.wsdl',
-                    retxml = True)
-    block_size = 100
-    block = take(block_size, seq_iter)
-    count = 0
-    while block:
-        for seq, gi in GetSeqs(client, block):
-            with open(os.path.join(dump_dir, gi + '.gi'), 'w') as handle:
-                handle.write('>%s\n%s\n' % (gi, seq))
-        count += len(block)
-        print count, len(dl_seqs)
-        block = take(block_size, seq_iter)
+    for seq, gi in GetSeqs(dl_seqs):
+        with open(os.path.join(dump_dir, gi + '.gi'), 'w') as handle:
+            handle.write('>%s\n%s\n' % (gi, seq))
 
     sh('touch %s' % out_file)
 
-######Utility Functions!
 
-
-
-
-
-
-
-def take(N, iterable):
-    return list(islice(iterable, N))
 
 
 if __name__ == '__main__':
