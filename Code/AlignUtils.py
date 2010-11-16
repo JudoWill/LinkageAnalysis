3ebd28e0ea1777efcc7655749d0f6de1513e22a7
Will Dampier <judowill@gmail.com>
2010-11-16 11:49:20 -0500

    added some alignment utilities to call clustalw

---
+++ Code/AlignUtils.py
@@ -0,0 +1,34 @@
+from collections import deque
+from GeneralUtils import *
+from subprocess import call
+import shlex
+
+def run_clustalw(filenames, out_fasta, out_tree, out_align):
+    
+    join_fasta(filenames, out_fasta)
+    info = {   
+            'ifile':out_fasta,
+            'tree':out_tree,
+            'ofile':out_align
+            }
+    cmd = 'clustalw -INFILE=%(ifile)s -QUICKTREE -NEWTREE=%(tree)s -OUTFILE=%(ofile)s -ENDGAPS -QUIET'
+    args = shlex.split(cmd % info)
+    call(args)
+    
+    cmd = 'clustalw -INFILE=%(ifile)s -QUICKTREE -USETREE=%(tree)s -OUTFILE=%(ofile)s -ENDGAPS -QUIET'
+    args = shlex.split(cmd % info)
+    call(args)
+
+def join_alignments(out_align, *aligns):
+    
+    base_align = aligns[0]
+    for align in aligns[1:]:
+        info = {
+                'align1':base_align,
+                'align2':align,
+                'out':out_align
+                }
+        cmd = 'clustalw -PROFILE1=%(align1)s -PROFILE2=%(align2)s -QUICKTREE -OUTFILE=%(out)s -ENDGAPS -PROFILE -QUIET'
+        args = shlex.split(cmd % info)
+        call(args)
+        base_align = out_align
