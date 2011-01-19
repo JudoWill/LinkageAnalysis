2c8d2c51f028a811984e0aad4fbeb7b04841d719
Will Dampier <dampier.william@gmail.com>
2011-01-19 15:55:22 -0500

    added a helper function for getting stuff from files

--- Code/StatUtils.py
+++ Code/StatUtils.py
@@ -1,12 +1,13 @@
 from __future__ import division
-import time
+import time, csv, os.path
 from math import log, exp
 #from mpmath import loggamma
-from operator import itemgetter, ge
+from operator import itemgetter, gt, ge
 from functools import partial
-from itertools import imap, starmap, repeat
+from itertools import imap, starmap, repeat, groupby, ifilter, tee, izip
 from random import shuffle
-from Code.AlignUtils import prediction_mapping
+from Code.AlignUtils import prediction_mapping, Alignment
+from Code.GeneralUtils import repeatfunc, unique_justseen, prots_from_path
 
 try:
     from memorised.decorators import memorise
@@ -30,12 +31,7 @@ except ImportError:
         def __get__(self, obj, objtype):
             return partial(self.__call__, obj)
 
-def repeatfunc(func, times , *args):
-    print times
-    print args
-    if times is None:
-        return starmap(func, repeat(args))
-    return starmap(func, repeat(args, times))
+
 
 def tuple_shuffle(tup):
     t = list(tup)
@@ -50,8 +46,49 @@ def linkage_pval(sa, sb, num_reps = 100000):
         return sum(imap(itemgetter(2), mappings))/len(signal_a)
     
     rscore = get_score(sa, sb)
-    check_score = partial(ge, rscore)
+    check_score = partial(gt, rscore)
     return sum(imap(check_score, repeatfunc(get_score, num_reps, sa, sb)))/num_reps
+
+def check_linkage_pval(a1, a2, ranges, num_reps = 100000):
+    
+    splitter = itemgetter(2,3)
+    for key, rows in groupby(ranges, itemgetter(0,1)):
+        a1s = a1.get_slice(*map(int, key))
+        a1names = set(a1s.seqs.keys())
+        for row in imap(splitter, rows):
+            a2s = a2.get_slice(*map(int, row))
+            names = a1names & set(a2s.seqs.keys())
+            
+            s1, _ = a1s.get_signal(sorted(names))
+            s2, _ = a2s.get_signal(sorted(names))
+            res = linkage_pval(tuple(s1), tuple(s2), num_reps = num_reps)
+            
+            yield key, row, res
+
+def check_linkage_file(filename, alignment_dir):
+    
+    p1, p2 = prots_from_path(filename)
+    
+    sorter = itemgetter('Source-Start', 'Source-End', 
+                        'Target-Start', 'Target-End')
+    handle = open(filename)
+    reader = csv.DictReader(handle, delimiter = '\t')
+    freader = ifilter(itemgetter('Total-Score'), reader)
+    good_rows = unique_justseen(freader, key = sorter)
+    i1, i2 = tee(good_rows)
+    ranges = imap(sorter, i2)
+    
+    a1 = Alignment.alignment_from_file(os.path.join(alignment_dir, p1+'.aln'))
+    a2 = Alignment.alignment_from_file(os.path.join(alignment_dir, p2+'.aln'))
+    
+    for row, (_, _, p) in izip(i1, check_linkage_pval(a1, a2, ranges)):
+        row['Source-Prot'] = p1
+        row['Target-Prot'] = p2
+        row['p-val'] = p
+        
+        yield row
+    
+    
 
 
 PVAL_CUT = 0.05
