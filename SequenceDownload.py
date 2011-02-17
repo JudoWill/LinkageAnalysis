e7faecf9627753e738222bf704c999b45448d702
Will Dampier <judowill@gmail.com>
2011-02-17 12:05:06 -0500

    re-modulated the code .... now much simpler

--- SequenceDownload.py
+++ SequenceDownload.py
@@ -2,52 +2,68 @@ from urllib2 import urlopen
 from urlparse import urljoin
 from collections import defaultdict
 from itertools import izip, repeat, chain, product
-import csv, argparse, os.path
+from operator import itemgetter
+import csv, argparse, os.path, logging
 
+
+
+def get_directories(url, check_terms):
+    handle = urlopen(url)
+    for row in csv.reader(handle, delimiter = ' '):
+        for term in check_terms:
+            if row[0].startswith('d') and row[-1].startswith(term):
+                yield term, url + '/' + row[-1]
+
+def get_file_urls(url):
+    wanted_extensions = ('.ptt', '.faa', '.ptt.tgz', '.faa.tgz')
+    handle = urlopen(url)
+    filenames = map(itemgetter(-1), csv.reader(handle, delimiter = ' '))
+    for fname, ext in product(filenames, wanted_extensions):
+        if fname.endswith(ext) and 'scaffold' not in fname:
+            yield url + '/' + fname
+
+def download_file(url, dump_dir):
+    
+    fname = url.rsplit('/',1)[-1]
+    with open(os.path.join(dump_dir, fname), 'w') as handle:
+        handle.write(urlopen(url).read())
+
+def check_NCBI(search_dict, urls):
+
+    check_locs = defaultdict(list)
+    logging.warning('Getting base directories')
+    for base_url in urls:
+        for term, url in get_directories(base_url, search_dict.keys()):
+            check_locs[term].append(url)
+    
+    for term, urls in check_locs.items():
+        dump_dir = search_dict[term]
+        logging.warning('Fetching %i genomes for %s' % (len(urls), term))
+        for url in urls:
+            logging.warning('Fetching: ' + url)
+            for furl in get_file_urls(url):
+                download_file(furl, dump_dir)
 
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Genomic Sequence Download Code')
-    parser.add_argument('--file', dest = 'file', type=str, help='file to check')
-    parser.add_argument('--no-download', dest = 'skipdownload', type=bool, action = 'store_true')
+    parser.add_argument('--file', dest = 'file', help='file to check')
+    parser.add_argument('--no-download', dest = 'skipdownload', default = False,
+                        action = 'store_true')
+    parser.add_argument('--no-drafts', dest = 'skipdraft', default = False,
+                        action = 'store_true')
     
     args = parser.parse_args()
 
-    base_url = 'ftp://ftp.ncbi.nih.gov/genomes/Bacteria/'
-    draft_url = 'ftp://ftp.ncbi.nih.gov/genomes/Bacteria_DRAFT/'
-    wanted_extensions = ('.ptt', '.faa', '.ptt.tgz', '.faa.tgz')
+    urls = ('ftp://ftp.ncbi.nih.gov/genomes/Bacteria/',)
+    if not args.skipdraft:
+        urls += ('ftp://ftp.ncbi.nih.gov/genomes/Bacteria_DRAFT/',)
     
     with open(args.file) as handle:
         search_dict = dict(csv.reader(handle))
         
-    base_handle = urlopen(base_url)
-    draft_handle = urlopen(draft_url)
-    iterable = chain(izip(repeat(base_url), csv.reader(base_handle, delimiter = ' ')), 
-                    izip(repeat(draft_url), csv.reader(draft_handle, delimiter = ' ')))
-    check_locs = defaultdict(list)
-
-    for url, row in iterable:
-        if row[0].startswith('d'):        
-            for term in search_dict.keys():        
-                if row[-1].startswith(term):
-                    check_locs[term].append(urljoin(url, row[-1]))
-
-    for term, urls in check_locs.items():
-        dump_dir = search_dict[term]
-        for url in urls:
-            print term, url
-            handle = urlopen(url)
-            rows = list(csv.reader(handle, delimiter = ' '))
-            for row, ext in product(rows, wanted_extensions):
-                if row[-1].endswith(ext) and 'scaffold' not in row[-1]:
-                    nfile = os.path.join(dump_dir, row[-1])
-                    if os.path.exists(nfile):
-                        continue
-                    nurl = url + '/' + row[-1]
-                    ihandle = urlopen(nurl)
-                    with open(nfile, 'w') as ohandle:
-                        ohandle.write(ihandle.read())
-                
+    if not args.skipdownload:
+        check_NCBI(search_dict, urls)
     
 
