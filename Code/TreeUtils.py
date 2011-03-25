4d30ae0cf954e92328bc6f78d4b5a173ac2aa443
Will Dampier <judowill@gmail.com>
2011-03-25 15:50:08 -0400

    added a larger function for running all phylip programs

--- Code/TreeUtils.py
+++ Code/TreeUtils.py
@@ -1,11 +1,48 @@
 from AlignUtils import Alignment
 from GeneralUtils import pushd
 from subprocess import call
-import shlex, os.path
+import shlex, os.path, os
+from StringIO import StringIO
 
 
 
-def run_proml(direc, input_args = ['Y'], capture_output = False, 
+
+def run_phylip(direc, progtype, input_args = ['y'], capture_output = False,
+                clean_direc = True):
+    """Runs the phylip suite of programs"""
+
+    with pushd(direc):
+        inbuf = StringIO('\n'.join(input_args+['']))
+        if progtype == 'proml':
+            check_files = ('infile', input_args[0])
+            rmfiles = ('outfile', 'outtree')
+            cmd = 'phylip proml'
+        elif progtype == 'consense':
+            check_files = ('intree', input_args[0])
+            rmfiles = ('outfile', 'outtree')
+            cmd = 'phylip consense'
+        else:
+            raise TypeError, 'Unrecognized phylip program: %s' % progtype
+
+        if clean_direc:
+            for f in outs:
+                try:
+                    os.remove(f)
+                except OSError:
+                    pass
+        
+        if capture_output:
+            out = open('screenout', 'w')
+        else:
+            out = open(os.devnull, 'w')
+
+        args = shlex.split(cmd)
+        call(args, stdin = inbuf, stdout = out)
+
+        
+
+
+def run_proml(direc, input_args = ['y'], capture_output = False, 
                 clean_direc = True):
    with pushd(direc):
         #make sure file is there
@@ -14,7 +51,7 @@ def run_proml(direc, input_args = ['Y'], capture_output = False,
             raise IOError, 'Could not find input file!'
 
         with open('input', 'w') as handle:
-            handle.write('\n'.join(input_args))
+            handle.write('\n'.join(input_args)+'\n')
 
         if clean_direc:
             outs = ('outfile', 'outtree')
@@ -24,14 +61,15 @@ def run_proml(direc, input_args = ['Y'], capture_output = False,
                 except OSError:
                     pass
 
-        cmd = 'phylip proml < input'
+        cmd = 'phylip proml'
         if capture_output:
-            cmd += ' > screenout'
+            out = open('screenout')
         else:
-            cmd += ' > /dev/null'
+            out = None
 
+        print cmd
         args = shlex.split(cmd)
-        call(args)
+        call(args, stdin = open('input'), stdout = out)
 
 def run_consense(direc, input_args = ['Y'], capture_output = False, 
                 clean_direc = True):
