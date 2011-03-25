5c4c7b7a97128742774d64872f77e30b0964f71b
Will Dampier <judowill@gmail.com>
2011-03-25 14:40:17 -0400

    added a function for consense

--- Code/TreeUtils.py
+++ Code/TreeUtils.py
@@ -7,7 +7,7 @@ import shlex, os.path
 
 def run_proml(direc, input_args = ['Y'], capture_output = False, 
                 clean_direc = True):
-    """Runs the phylip proml function."""
+
 
    with pushd(direc):
         #make sure file is there
@@ -34,3 +34,34 @@ def run_proml(direc, input_args = ['Y'], capture_output = False,
 
         args = shlex.split(cmd)
         call(args)
+
+def run_consense(direc, input_args = ['Y'], capture_output = False, 
+                clean_direc = True):
+    """Runs the phylip consense function."""
+
+   with pushd(direc):
+        #make sure file is there
+        fpresent = os.path.exists('intree') or os.path.exists(input_args[0].strip())
+        if not fpresent:
+            rasie IOError, 'Could not find input file!'
+
+        with open('input', 'w') as handle:
+            handle.write('\n'.join(input_args))
+        
+        if clean_direc:
+            outs = ('outfile', 'outtree')
+            for f in outs:
+                try:
+                    os.remove(f)
+                except OSError:
+                    pass
+
+        cmd = 'phylip consense < input'
+        if capture_output:
+            cmd += ' > screenout'
+        else:
+            cmd += ' > /dev/null'
+
+        args = shlex.split(cmd)
+        call(args)
+
