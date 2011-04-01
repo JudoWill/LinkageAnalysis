163d7d41fc8e32774a81279a1d123c23b5aa0285
Will Dampier <dampier.william@gmail.com>
2011-04-01 13:28:39 -0400

    made ALNDATA and ALNFILE into global variables since this will make things much eaiser when I need to change things later

--- testAlign.py
+++ testAlign.py
@@ -2,21 +2,19 @@ import nose
 from Code import AlignUtils
 import os, os.path
 
+ALNDATA = (('key1', 'AACAA'), ('key2', 'A-CAA'))
+ALNFILE = os.path.join(os.environ['TMPDIR'], 'tmp.aln')
 
 def aln_write_fun():
     
-    tfile = os.path.join(os.environ['TMPDIR'], 'tmp.aln')
-    indata = (('key1', 'AACAA'), ('key2', 'A-CAA'))
-    
-    with open(tfile, 'w') as handle:
-        for key, seq in indata:
+    with open(ALNFILE, 'w') as handle:
+        for key, seq in ALNDATA:
             handle.write('%s\t%s\n' % (key,seq))
     
 def aln_delete():
     
-    tfile = os.path.join(os.environ['TMPDIR'], 'tmp.aln')
     try:
-        os.remove(tfile)
+        os.remove(ALNFILE)
     except IOError:
         pass
 
@@ -27,45 +25,40 @@ def test_load_alignment():
 @nose.tools.with_setup(aln_write_fun, aln_delete)
 def test_load_alignment_from_file():
     
-    tfile = os.path.join(os.environ['TMPDIR'], 'tmp.aln')
-    
-    aln = AlignUtils.Alignment.alignment_from_file(tfile)
+    aln = AlignUtils.Alignment.alignment_from_file(ALNFILE)
     nose.tools.eq_(aln.width, 5)
-    indata = (('key1', 'AACAA'), ('key2', 'A-CAA'))
-    for key, seq in indata:
+    
+    for key, seq in ALNDATA:
         nose.tools.eq_(aln.seqs[key], seq)
 
 @nose.tools.with_setup(aln_write_fun, aln_delete)            
 def test_join_alignments_joining_sequences():
     
-    tfile = os.path.join(os.environ['TMPDIR'], 'tmp.aln')
-    aln1 = AlignUtils.Alignment.alignment_from_file(tfile)
-    aln2 = AlignUtils.Alignment.alignment_from_file(tfile) 
-    indata = (('key1', 'AACAA'), ('key2', 'A-CAA'))
+    aln1 = AlignUtils.Alignment.alignment_from_file(ALNFILE)
+    aln2 = AlignUtils.Alignment.alignment_from_file(ALNFILE)
     
     aln1.append_alignment(aln2)
     nose.tools.eq_(aln1.width, 10)
-    nose.tools.eq_(len(aln1.seqs), 2)
+    nose.tools.eq_(len(aln1.seqs), len(ALNDATA))
     
-    for key, seq in indata:
+    for key, seq in ALNDATA:
         nose.tools.eq_(aln1.seqs[key], seq+seq)    
         
 @nose.tools.with_setup(aln_write_fun, aln_delete)            
 def test_join_alignments_limiting_sequences():
     
-    tfile = os.path.join(os.environ['TMPDIR'], 'tmp.aln')
-    aln1 = AlignUtils.Alignment.alignment_from_file(tfile)
-    aln2 = AlignUtils.Alignment.alignment_from_file(tfile)
-    aln2.seqs['key3'] = 'ATCAA'
-    indata = (('key1', 'AACAA'), ('key2', 'A-CAA'))
+    aln1 = AlignUtils.Alignment.alignment_from_file(ALNFILE)
+    aln2 = AlignUtils.Alignment.alignment_from_file(ALNFILE)
+    aln2.seqs['junkkey'] = 'ATCAA'
+    
     
     aln1.append_alignment(aln2)
     nose.tools.eq_(aln1.width, 10)
     
-    for key, seq in indata:
+    for key, seq in ALNDATA:
         nose.tools.eq_(aln1.seqs[key], seq+seq)
         
-    nose.tools.eq_(len(aln1.seqs), 2)
+    nose.tools.eq_(len(aln1.seqs), len(ALNDATA))
         
         
         
