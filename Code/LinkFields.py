93c966309f30d890f37a9c816712c0aef262927f
Will Dampier <dampier.william@gmail.com>
2011-12-20 12:06:07 -0500

    fixed a stupid typo!

--- Code/LinkFields.py
+++ Code/LinkFields.py
@@ -3,4 +3,4 @@
 
 LINK_FIELDS = ('Source-Prot', 'Target-Prot','Source-Start', 'Source-End', 'Target-Start', 'Target-End',
                 'Source-Seq', 'Target-Seq', 'Correct-Num', 'Total-Num', 'This-Score', 'Total-Score', 
-                'Source-Cons', 'Target-Cons', 'Mutual-Info', 'PNAS-Dist', 'Null-Mutual-Info', 'Corrected-Mutaul-Info')
+                'Source-Cons', 'Target-Cons', 'Mutual-Info', 'PNAS-Dist', 'Null-Mutual-Info', 'Corrected-Mutual-Info')
