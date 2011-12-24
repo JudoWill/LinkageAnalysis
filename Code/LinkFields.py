a9f26e5f20c0c65c69d973e5ca81628f1bbead0c
Will Dampier <dampier.william@gmail.com>
2011-12-23 22:48:52 -0500

    forgot a few fields

--- Code/LinkFields.py
+++ Code/LinkFields.py
@@ -3,4 +3,4 @@ from LinkUtils import LinkCalculator
 
 LINK_FIELDS = ['Source-Prot', 'Target-Prot','Source-Start', 'Source-End', 'Target-Start', 'Target-End',
                 'Source-Seq', 'Target-Seq', 'Correct-Num', 'Total-Num', 'This-Score', 'Total-Score', 
-                'Source-Cons', 'Target-Cons'] + LinkCalculator().get_fields()
+                'Source-Cons', 'Target-Cons', 'Target-Entropy', 'Source-Entropy'] + LinkCalculator().get_fields()
