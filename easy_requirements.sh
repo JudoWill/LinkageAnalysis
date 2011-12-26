c3aa9c073091208851077dd686c148b1f0b6cbfb
Will Dampier <dampier.william@gmail.com>
2011-12-25 23:23:03 -0500

    added an easy_install script

---
+++ easy_requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+easy_install paver
+easy_install ruffus
+easy_install nose
+easy_install memorised
+easy_install python-memcached
+easy_install pyyaml
+easy_install BeautifulSoup
+easy_install suds
+easy_install dendropy
+easy_install pylru
+easy_install redis
+easy_install celery
