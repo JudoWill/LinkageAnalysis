85c9e94d743df8793f03553e0ee22c7a3685f8df
Will Dampier <dampier.william@gmail.com>
2012-01-03 17:22:22 -0500

    now creates logfiles

--- checkprocess.py
+++ checkprocess.py
@@ -1,6 +1,7 @@
 __author__ = 'will'
 from Code.CeleryProcessor import PredictionAnalysis
 import os, os.path
+import logging
 
 
 if __name__ == '__main__':
@@ -11,4 +12,6 @@ if __name__ == '__main__':
     for tfun in testfuns:
         ofile = '/hivdata/test/Env--Env.aln.'+tfun
         if not os.path.exists(ofile):
+            fname = 'processing-%s.log' % tfun
+            logging.basicConfig(filename=fname,level=logging.DEBUG)
             PredictionAnalysis(basealign, basealign, ofile, granular=True, limit_functions=set([tfun]))
